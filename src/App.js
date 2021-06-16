30154685c9a4e0aff9c5c13ad9c7646376f8a721
Piyush Verma <61598060+piyushverma-coder@users.noreply.github.com>
2021-06-17 00:01:14 +0530

    style changes converted draw prerder to postorder for lines small style changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import BST from './BST';
 import Canvas from './Canvas';
 
 export default class App extends Component {
@@ -10,10 +9,18 @@ export default class App extends Component {
     // [15,25,10,7,22,17,13,5,9,27]
   }
   handleAdd() {
-    const data = document.querySelector('#add').value;
-    console.log(data);
+    var data = document.querySelector('#add').value;
     const input = this.state.input;
-    input.push(data);
+    data=parseInt(data);
+    if(!data)return
+    input.push(parseInt(data));
+    this.setState({input});
+  }
+  handleAddMultiple(){
+    var str = document.querySelector('#addM').value;
+    var input=str.split(' ')
+    for(let i=0;i<input.length;i++)
+    input[i]=parseInt(input[i]);
     this.setState({input});
   }
   render() {
@@ -22,7 +29,17 @@ export default class App extends Component {
         <div id="controls">
           <div>
             <input id="add" type="number"></input>
-            <button onClick={(e) => this.handleAdd()}>add</button>
+            <button onClick={(e) => this.handleAdd()} style={{
+              borderTopRightRadius:7,
+              borderBottomRightRadius:7
+            }}>add</button>
+          </div>
+          <div>
+            <input id="addM" type="text" defaultValue="15 25 10 7 22 17 13 5 9 27"/>
+            <button onClick={(e) => this.handleAddMultiple()} style={{
+              borderTopRightRadius:7,
+              borderBottomRightRadius:7
+            }} >add multiple</button>
           </div>
           <div>
             <button onClick={(e) => this.setState({input:[]})}>reset</button>
